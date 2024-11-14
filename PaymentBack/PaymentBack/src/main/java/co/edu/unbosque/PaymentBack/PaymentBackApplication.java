e6e5541ee9fb320010e7684221b1529e6c86bf7e
JuanOyolaGalindo <114842542+JuanOyolaGalindo@users.noreply.github.com>
2024-11-13 20:51:56 -0500

    Add files via upload

---
+++ PaymentBack/PaymentBack/src/main/java/co/edu/unbosque/PaymentBack/PaymentBackApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.PaymentBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentBackApplication.class, args);
+	}
+
+}
